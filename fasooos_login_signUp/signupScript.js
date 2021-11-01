988eaf61af509c0219a2ddfa667b87a9bbcfa09f
sumansaksh <91533089+sumansaksh@users.noreply.github.com>
2021-11-01 23:32:21 +0530

    Update signupScript.js

--- fasooos_login_signUp/signupScript.js
+++ fasooos_login_signUp/signupScript.js
@@ -77,12 +77,13 @@ let arr = JSON.parse(localStorage.getItem("faasosUser"))
 arr.forEach(el => {
 
    if(number==el.number || email==el.email){
-       alert("wrong credentials")
+       alert("user exist")
        credentials = false
    }
 });
 if(credentials==true){
 arr.push(user)
+  alert("Sign Up successful")
 }
            
 
