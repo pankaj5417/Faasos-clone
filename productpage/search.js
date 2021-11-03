ee4e4d5817c46b3c751b918bedbe5babafe9db27
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-03 10:21:43 +0530

    update
    seacrh bar now working properly

--- productpage/search.js
+++ productpage/search.js
@@ -2,10 +2,8 @@
 
 function search(){
     document.getElementById("searchModal").style.display = "block";
-    document.getElementById("search-container").style.display = "none";
 }
 let close = document.getElementById("close");
 close.addEventListener("click",() => {
     document.getElementById("searchModal").style.display = "none";
-    document.getElementById("search-container").style.display = "block";
 });
