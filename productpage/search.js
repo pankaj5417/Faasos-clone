17c16c2d250458a638481c4e6b4304f607f8a698
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-03 15:03:11 +0530

    update in search
    API is working.

--- productpage/search.js
+++ productpage/search.js
@@ -7,3 +7,14 @@ let close = document.getElementById("close");
 close.addEventListener("click",() => {
     document.getElementById("searchModal").style.display = "none";
 });
+async function getData() {
+  let res=await fetch(`https://demo8715768.mockable.io/faasos`);
+  let data=await res.json();
+  console.log("My Data:",data);
+  dataSearch(data);
+} 
+getData();
+
+function dataSearch(){
+  
+}
