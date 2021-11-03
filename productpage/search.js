c48604884d39fc6bdf9d02bb06a4353f616ee5ab
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-03 08:56:14 +0530

    Minor Updates

--- productpage/search.js
+++ productpage/search.js
@@ -1,12 +1,11 @@
-var searchContainer=document.getElementById("search-container")
+
 
 function search(){
     document.getElementById("searchModal").style.display = "block";
-    searchContainer.style.display="none"
+    document.getElementById("search-container").style.display = "none";
 }
 let close = document.getElementById("close");
 close.addEventListener("click",() => {
     document.getElementById("searchModal").style.display = "none";
-    searchContainer.style.display="block"
-
+    document.getElementById("search-container").style.display = "block";
 });
