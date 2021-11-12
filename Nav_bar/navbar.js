c6f6bb11979c84a229c32ed48f4f9eaeb6a662be
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-12 14:42:40 +0530

    update
    ham menu is working properly.
    anoutus and help & support pages link are added

--- Nav_bar/navbar.js
+++ Nav_bar/navbar.js
@@ -7,5 +7,34 @@ const nav = `<div id="navLeft">
   <div>Login</div>
 </div>
 <span id="ham" class="material-icons">menu</span>
+</div>`;
+const sid = `<span id="closeBtn" class="material-icons">close</span>
+<div class="prof">
+  <span class="material-icons">account_circle</span>
+  <div>Profile</div>
+</div>
+<div class="prof">
+  <span class="material-icons">border_color</span>
+  <div>Manage Orders</div>
+</div>
+<div class="prof">
+  <span class="material-icons">location_on</span>
+  <div>Manage Adresses</div>
+</div>
+<div class="prof">
+  <span class="material-icons">currency_rupee</span> 
+  <div>Manage Payments</div>
+</div>
+<div class="prof">
+  <span class="material-icons">contact_support</span>
+  <div id="help">Help & Support</div>
+</div>
+<div class="prof">
+  <span class="material-icons">description</span>
+  <div id="about">About Us</div>
+</div>
+<div class="prof">
+  <span class="material-icons">celebration</span>
+  <div>Party Orders</div>
 </div>`
-export default nav;
+export  {nav,sid};
