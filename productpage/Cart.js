13891a310b1cc681af97c63894cf6b9711336927
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-13 13:11:07 +0530

    update
    minor changes links are working well

--- productpage/Cart.js
+++ productpage/Cart.js
@@ -1,3 +1,8 @@
+/*preloader*/ 
+var preloader = document.getElementById("loading");
+document.body.onload = () => {
+  preloader.style.display = "none"; 
+}
 import { product_type, customize } from "./export.js";
 
 import {nav,sid} from "../Nav_bar/navbar.js";
@@ -22,11 +27,6 @@ async function getData(){
 } 
 getData()
 
-/*preloader*/ 
-var preloader = document.getElementById("loading");
-document.body.onload = () => {
-  preloader.style.display = "none"; 
-}
 
 var viewCouponsFun=document.getElementById("view-coupons")
 viewCouponsFun.onclick=()=>{
